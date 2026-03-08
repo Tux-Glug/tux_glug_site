2041f586fbe06c582e66e97746133bf1758ec94d
Jaz0XD <jazwastaken.xd@gmail.com>
2026-03-08 17:31:14 +0530

    Updates

--- Client/src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-8 border-t border-gray-800 text-gray-500">
-      © 2026 TUX GLUG. All rights reserved.
-    </footer>
-  );
-}
