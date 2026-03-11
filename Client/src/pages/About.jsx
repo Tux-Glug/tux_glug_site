7cb99efebdb40c1b1085fe173e507ded337421e7
Jaz0XD <jazwastaken.xd@gmail.com>
2026-03-11 20:14:34 +0530

    added new Office bearers and EC

--- Client/src/pages/About.jsx
+++ Client/src/pages/About.jsx
@@ -133,35 +133,37 @@ export default function About() {
           <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
 
             {/* Member 1 */}
+            {/*PRESIDENT */}
             <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
               <img
-                src="/tux_glug_site/images/office-bearers/Sirajuddeen-pred.jpg"
+                src="/tux_glug_site/images/office-bearers/Karneish.jpeg"
                 alt="President"
                 className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
               />
               <h3 className="text-xl text-green-400 mb-2">
-                MOHAMED B SIRAJUDDEEN
+                KARNEISH
               </h3>
               <p className="text-gray-400 text-sm">
                 President
               </p>
               <p className="text-gray-500 text-sm">
-                CSE — Batch 2022–2026
+                CSE — Batch 2023–2027
               </p>
             </div>
 
             {/* Member 2 */}
+            {/*VICE-PRESIDENT */}
             <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
               <img
-                src="/tux_glug_site/images/office-bearers/Aatish-sec.jpeg"
-                alt="Secretary"
+                src="/tux_glug_site/images/office-bearers/Kavin.jpeg"
+                alt="Vice-President"
                 className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
               />
               <h3 className="text-xl text-green-400 mb-2">
-                AATISH ASHOK
+                KAVIN VENTHAN J
               </h3>
               <p className="text-gray-400 text-sm">
-                Secretary
+                Vice-President
               </p>
               <p className="text-gray-500 text-sm">
                 CSE — Batch 2023–2027
@@ -169,14 +171,35 @@ export default function About() {
             </div>
 
             {/* Member 3 */}
+            {/*SECRETARY */}
             <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
               <img
-                src="/tux_glug_site/images/office-bearers/Kavin-jt-sec.jpeg"
+                src="/tux_glug_site/images/office-bearers/Aatish.jpeg"
+                alt="Secretary"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                AATISH ASHOK A
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Secretary<br></br>
+                Designer Team Head
+              </p>
+              <p className="text-gray-500 text-sm">
+                CSE — Batch 2023–2027
+              </p>
+            </div>
+
+            {/* Member 4 */}
+            {/*JOINT-SECRETARY */}
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/office-bearers/Pragadeeswar.jpeg"
                 alt="Joint-Secretary"
                 className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
               />
               <h3 className="text-xl text-green-400 mb-2">
-                KAVIN VENTHAN
+                PRAGADEESWAR S
               </h3>
               <p className="text-gray-400 text-sm">
                 Joint Secretary
@@ -186,18 +209,38 @@ export default function About() {
               </p>
             </div>
 
-            {/* Member 4 */}
+            {/* Member 5 */}
+            {/*JOINT-SECRETARY */}
             <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
               <img
-                src="/tux_glug_site/images/office-bearers/Karneish-jt-sec.jpeg"
-                alt="Secretary"
+                src="/tux_glug_site/images/office-bearers/JayaBeulah.jpeg"
+                alt="Joint-Secretary"
                 className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
               />
               <h3 className="text-xl text-green-400 mb-2">
-                KARNEISH
+                JAYA BEULAH
               </h3>
               <p className="text-gray-400 text-sm">
                 Joint Secretary
+              </p>
+              <p className="text-gray-500 text-sm">
+                CSE — Batch 2023–2027
+              </p>
+            </div>
+
+            {/* Member 6 */}
+            {/* TREASURER */}
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/office-bearers/Sanjay.jpeg"
+                alt="Treasurer"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                SANJAY T
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Treasurer
               </p>
               <p className="text-gray-500 text-sm">
                 CSE — Batch 2023–2027
@@ -207,6 +250,134 @@ export default function About() {
           </div>
         </div>
 
+
+
+        {/* Executive Committee */}
+        <div>
+          <h2 className="text-3xl text-green-400 mt-16 mb-16">
+            Executive Committee
+          </h2>
+
+          <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
+
+            {/* Member 1 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/Karneish.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                RAVI KUMAR MS
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member
+              </p>
+              <p className="text-gray-500 text-sm">
+                CSE — Batch 2023–2027
+              </p>
+            </div>
+
+            {/* Member 2 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/Kavin.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                VIDHYASAAGAR
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member
+              </p>
+              <p className="text-gray-500 text-sm">
+                CSE — Batch 2023–2027
+              </p>
+            </div>
+
+            {/* Member 3 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/Kavin.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                RITHIK RATHAN C
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member <br></br>
+                Developer Team Head
+              </p>
+              <p className="text-gray-500 text-sm">
+                CSE — Batch 2023–2027
+              </p>
+            </div>
+            {/* Member 4 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/Aatish.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                GAJINI P
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member
+              </p>
+              <p className="text-gray-500 text-sm">
+                AI&DS — Batch 2024–2028
+              </p>
+            </div>
+
+            {/* Member 5 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/Pragadeeswar.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                GOWTHAM V
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member
+              </p>
+              <p className="text-gray-500 text-sm">
+                AI&DS — Batch 2024–2028
+              </p>
+            </div>
+
+            {/* Member 6 */}
+
+            <div className="border border-gray-800 rounded-xl p-6 hover:border-green-400 transition duration-300">
+              <img
+                src="/tux_glug_site/images/executive-committee/JayaBeulah.jpeg"
+                alt="EC-Member"
+                className="w-32 h-32 mx-auto rounded-full object-cover mb-6"
+              />
+              <h3 className="text-xl text-green-400 mb-2">
+                KISHORE
+              </h3>
+              <p className="text-gray-400 text-sm">
+                Executive Member
+              </p>
+              <p className="text-gray-500 text-sm">
+                AI&DS — Batch 2024–2028
+              </p>
+            </div>
+
+
+
+          </div>
+        </div>
       </div>
     </section>
   );
